f68cbb3b34dd26980c2335354bbb748ddbb321a1
inverse555 <inverse555@outlook.jp>
2026-01-03 01:43:55 +0900

    Add window class in init script.

--- src/picmaker.py
+++ src/picmaker.py
@@ -11,26 +11,62 @@ from picmaker_reverse import PicMakerReverse
 from picmaker_tw import PicMakerTW
 
 
+class mode_window:
+    """
+    初期ウィンドウ管理クラス
+    """
+
+    def __init__(self):
+        """
+        コンストラクタ
+        """
+        self.flag_exe_main = True
+
+        self.tk_root = tkinter.Tk()
+        self.tk_root.protocol("WM_DELETE_WINDOW", self.on_close_mode_window)
+        self.tk_root.title("picmaker - モード選択")
+        ttk.Label(self.tk_root, text="モード").grid(row=0, column=0, padx=6, pady=6, sticky="w")
+        mode_options = ["Reverse", "The World"]
+        self.combo_modes = tkinter.StringVar(value=mode_options[0])
+        combo = ttk.Combobox(
+            self.tk_root,
+            textvariable=self.combo_modes,
+            values=mode_options,
+            state="readonly",
+            width=10,
+        )
+        combo.grid(row=0, column=1, padx=6, pady=6, sticky="w")
+        button_ok = ttk.Button(self.tk_root, text="OK", command=self.on_ok_mode_window)
+        button_ok.grid(row=0, column=2, padx=6, pady=6, sticky="w")
+
+    def on_ok_mode_window(self) -> None:
+        """
+        モード選択ウィンドウ OK 時のハンドラ
+        """
+        self.tk_root.destroy()
+
+    def on_close_mode_window(self) -> None:
+        """
+        モード選択ウィンドウクローズ時のハンドラ
+        """
+        self.on_ok_mode_window()
+        self.flag_exe_main = False
+
+    def entrypoint(self) -> None:
+        """
+        エントリポイント
+        """
+        self.tk_root.mainloop()
+
+    @property
+    def mode(self) -> str:
+        return self.combo_modes.get()
+
+
 def main() -> None:
     """
     エントリポイント
     """
-
-    flag_exe = True
-
-    def on_ok_mode_window() -> None:
-        """
-        モード選択ウィンドウ OK 時のハンドラ
-        """
-        tk_root.destroy()
-
-    def on_close_mode_window() -> None:
-        """
-        モード選択ウィンドウクローズ時のハンドラ
-        """
-        nonlocal flag_exe
-        on_ok_mode_window()
-        flag_exe = False
 
     parser = argparse.ArgumentParser(
         prog="picmaker.py",
@@ -49,29 +85,13 @@ def main() -> None:
         elif args.mode == "R":
             pm = PicMakerReverse()
         else:
-            tk_root = tkinter.Tk()
-            tk_root.protocol("WM_DELETE_WINDOW", on_close_mode_window)
-            tk_root.title("picmaker - モード選択")
-            ttk.Label(tk_root, text="モード").grid(row=0, column=0, padx=6, pady=6, sticky="w")
-            mode_options = ["Reverse", "The World"]
-            combo_modes = tkinter.StringVar(value=mode_options[0])
-            combo = ttk.Combobox(
-                tk_root,
-                textvariable=combo_modes,
-                values=mode_options,
-                state="readonly",
-                width=10,
-            )
-            combo.grid(row=0, column=1, padx=6, pady=6, sticky="w")
-            button_ok = ttk.Button(tk_root, text="OK", command=on_ok_mode_window)
-            button_ok.grid(row=0, column=2, padx=6, pady=6, sticky="w")
-            tk_root.mainloop()
-            if not flag_exe:
+            window = mode_window()
+            window.entrypoint()
+            if not window.flag_exe_main:
                 return
-
-            if combo_modes.get() == "The World":
+            elif window.mode == "The World":
                 pm = PicMakerTW()
-            elif combo_modes.get() == "Reverse":
+            elif window.mode == "Reverse":
                 pm = PicMakerReverse()
         if pm is not None:
             signal.signal(signal.SIGINT, pm.sigint_handler)
